4e5c746099418a1ab1025889370c256e572e3ba9
ne_skrooomnaya <angelf04032005@gmail.com>
2022-09-25 18:11:03 +0300

    проверка_13/test

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use((req, res, next) => {
   next();
 });
 
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
 app.use(express.json());
 
 app.use(UserRoutes);
@@ -30,25 +35,27 @@ app.use((req, res) => {
   res.status(ErrorNot).send({ message: "Произошла ошибка" });
 });
 
-async function main() {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/mestodb", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    await app.listen(PORT);
+    app.use(express.static(path.join(__dirname, "public")));
+    app.listen(PORT, () => {
     console.log(`Сервер запущен на ${PORT} порту`);
-  } catch (err) {
-    console.log(err);
-  }
-}
+  });
 
-main();
+
+// function main() {
+//   try {
+//     await
+//     await
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// main();
 
 // useCreateIndex: true,
 // useFindAndModify: true,
 
-// app.use(express.static(path.join(__dirname, "public")));
+//
 
 //   app.use((req, res, next) => {
 //     req.card = {
