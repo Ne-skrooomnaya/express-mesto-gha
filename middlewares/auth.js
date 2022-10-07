e5f2ca8bd8ea60edf649bee4299bf49e36049c46
ne_skrooomnaya <angelf04032005@gmail.com>
2022-10-07 23:56:46 +0300

    проверка_14/test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,10 +2,10 @@
 const jwt = require('jsonwebtoken');
 const { ErrorUnauthorized } = require('../utils/ErrorUnauthorized');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new ErrorUnauthorized('Необходима авторизация 1');
+    return res.status(ErrorUnauthorized).send({ message: 'Необходима авторизация 3' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw new ErrorUnauthorized('Необходима авторизация 2');
+    return res.status(ErrorUnauthorized).send({ message: 'Необходима авторизация 4' });
   }
 
   req.user = payload;
