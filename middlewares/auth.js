de2f7cc2f948b10aac0d999eba971546afa28109
ne_skrooomnaya <angelf04032005@gmail.com>
2022-10-07 20:18:23 +0300

    проверка_14/test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ const {
 module.exports = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new ErrorUnauthorized('Необходима авторизация 1');
+    return res.status(ErrorUnauthorized).send({ message: 'Необходима авторизация 3' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
@@ -15,7 +15,7 @@ module.exports = async (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw new ErrorUnauthorized('Необходима авторизация 2');
+    return res.status(ErrorUnauthorized).send({ message: 'Необходима авторизация 4' });
   }
 
   req.user = payload;
--- utils/errors.js
+++ utils/errors.js
@@ -1,10 +1,5 @@
 const ErrorBad = 400;
-class ErrorUnauthorized extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 401;
-  }
-}
+const ErrorUnauthorized = 401;
 const ErrorForbidden = 403;
 const ErrorNot = 404;
 const ErrorConflict = 409;
