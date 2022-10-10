6b4afbee7ef38856985151071d4be367b42114d2
ne_skrooomnaya <angelf04032005@gmail.com>
2022-10-11 00:09:54 +0300

    проверка_14/test

--- models/User.js
+++ models/User.js
@@ -1,9 +1,8 @@
 /* eslint-disable func-names */
 const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
-const isUrl = require('validator/lib/isURL');
-const isEmail = require('validator/lib/isEmail');
-// const { ErrorUnauthorized } = require('../utils/ErrorUnauthorized');
+// const isUrl = require('validator/lib/isURL');
+// const isEmail = require('validator/lib/isEmail');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,18 +22,22 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    required: [true, 'Поле "avatar" должно быть заполнено'],
     validate: {
-      validator: (url) => isUrl(url),
-      message: 'Ссылка указана неверно',
+      validator(v) {
+        return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]+\.[a-zA-Z0-9()]+([-a-zA-Z0-9()@:%_\\+.~#?&/=#]*)/.test(v);
+      },
+      message: 'Невалидная ссылка',
     },
   },
+
   email: {
     type: String,
-    required: true,
     unique: true,
+    required: [true, 'Поле "email" должно быть заполнено'],
     validate: {
-      validator: (email) => isEmail(email),
-      message: 'Почта указана неверно',
+      validator: validator.isEmail,
+      message: 'Неверно заполнен email',
     },
   },
   password: {
@@ -44,21 +47,5 @@ const userSchema = new mongoose.Schema({
     minlenght: 8,
   },
 });
-
-// userSchema.statics.findUserByCredentials = function (email, password) {
-//   return this.findOne({ email }).select('+password')
-//     .then((user) => {
-//       if (!user) {
-//         throw new ErrorUnauthorized('Неправильные почта или пароль');
-//       }
-//       return bcrypt.compare(password, user.password)
-//         .then((matched) => {
-//           if (!matched) {
-//             throw new ErrorUnauthorized('Неправильные почта или пароль');
-//           }
-//           return user;
-//         });
-//     });
-// };
 
 module.exports = mongoose.model('User', userSchema);
