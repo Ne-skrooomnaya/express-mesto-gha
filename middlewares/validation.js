e7c47d932a1f8c556acae7aca62abf1378e09ef3
ne_skrooomnaya <angelf04032005@gmail.com>
2022-10-11 00:22:44 +0300

    проверка_14/test

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,13 +1,12 @@
 const { celebrate, Joi } = require('celebrate');
 const validator = require('validator');
 
-const validateUrl = ((value) => {
+const method = (value) => {
   const result = validator.isURL(value);
-  if (!result) {
-    throw new Error('URL validation err');
-  }
-  return value;
-});
+  if (result) {
+    return value;
+  } throw new Error('URL validation err');
+};
 
 const authValidation = celebrate({
   body: Joi.object().keys({
@@ -18,9 +17,11 @@ const authValidation = celebrate({
 
 const registerValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().custom(validateUrl),
+    name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
+    about: Joi.string().min(2).max(30).default('Исследователь'),
+    avatar: Joi.string()
+      .custom(method)
+      .default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
@@ -28,33 +29,33 @@ const registerValidation = celebrate({
 
 const userValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 });
 
 const userIdValidation = celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24).hex(),
+    userId: Joi.string().length(24).hex(),
   }),
 });
 
 const avatarValidation = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().custom(validateUrl),
+    avatar: Joi.string().required().custom(method),
   }),
 });
 
 const cardValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().custom(validateUrl),
+    link: Joi.string().required().custom(method),
   }),
 });
 
 const cardIdValidation = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24).hex(),
+    cardId: Joi.string().required().length(24).hex(),
   }),
 });
 
